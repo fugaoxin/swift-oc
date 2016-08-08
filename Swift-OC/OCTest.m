43c03de28fcd2039a83ec6a5f05abb6ea3fcc41f
123 <123@SINO-2015-0192-GZ-GaoXin-Fu.local>
2016-08-08 10:49:54 +0800

    new project

---
+++ Swift-OC/OCTest.m
@@ -0,0 +1,40 @@
+//
+//  OCTest.m
+//  Swift-OC
+//
+//  Created by 123 on 16/7/28.
+//  Copyright © 2016年 123. All rights reserved.
+//
+
+#import "OCTest.h"
+
+@interface OCTest ()
+
+@end
+
+@implementation OCTest
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+-(void)test{
+   NSLog(@"hello!! welcome to OC");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
